d53bde43245ec1de19f1b763e616d6795af6a831
maher <mahtrabelsi@gmail.com>
2023-11-14 18:35:43 +0100

    creation du projet

---
+++ spring_boot_internationalisation/src/main/java/org/i18n/me/InternationalisationApplication.java
@@ -0,0 +1,13 @@
+package org.i18n.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternationalisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternationalisationApplication.class, args);
+	}
+
+}
---
+++ spring_boot_internationalisation/src/test/java/org/i18n/me/internationalisationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.i18n.me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class internationalisationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
